52183348ac46afe87f3a4d6fd60b73925068c66f
Cale McNulty <cale.mcnulty@gauntletai.com>
2025-08-29 21:18:31 -0500

    fix: add missing Button import in Sidebar component

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -8,6 +8,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 interface SidebarProps {
   className?: string;
